efa9e304b82ae92a61cdd5466bdef64822266d2d
Gaarazinho 愛 <77336228+Sasukezin@users.noreply.github.com>
2021-02-11 03:45:09 +0300

    Add files via upload

---
+++ src/erenmenu.js
@@ -0,0 +1,12 @@
+const erenmenu = (prefix, pushname) => {
+    return `◪ *Comandos do Eren*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.erenmenu = erenmenu
